1a8f6a7504bcab1b1992d50139ae307c5efdb021
Denis Doria <denisdoria@gmail.com>
2011-08-13 19:41:52 -0300

    Added blink effect when die. Added 'apple in stomach' effect. Included difficult (increase and decrease). Included restart. Improved the creation of apples. Improved the snake movement (now using a update_snake, instead of create_snake).
    Many small changes in the code to be able to add features easily

--- pysnake.py
+++ pysnake.py
@@ -1,31 +1,38 @@
 import curses
+import curses.ascii
 import time
 import random
-import string
 import sys
-import math
 
 def clean_snake(win, snake):
     for i in snake:
         win.addch(i[0], i[1], ' ')
+    win.refresh()
+        
+def update_snake(win, snake):
+    win.addch(snake[0][0], snake[0][1], '#')
+    win.addch(snake[-1][0], snake[-1][1], ' ')
+    win.refresh()
 
 def create_snake(win, snake):
     for i in snake:
         win.addch(i[0], i[1], '#')
+    win.refresh()
         
 def create_apple(win):
     height, width = win.getmaxyx()
     y = -1
     x = -1
-    while not (2 < y < height - 1 and 2 < x < width - 1):
-                y = int(random.random() * height)
-                x = int(random.random() * width)               
-    
+                
+    while win.inch(y, x) != ord(' '):
+        y = int(random.random() * height)
+        x = int(random.random() * width)
+        
     win.addch(y, x, '@', curses.color_pair(1))
     win.refresh()
-    
+
     return y, x
-    
+
 def main(stdscr_d):
     curses.init_pair(1, curses.COLOR_RED, 0)
     stdscr = stdscr_d.subwin(30, 40, 0, 0)
@@ -34,8 +41,9 @@ def main(stdscr_d):
 
     difficult = 1
     score = 0
-    snake = [(10, 10),(10,9),(10,8)]
-    stdscr.addch(snake[0][0], snake[0][1] , '#')
+    
+    snake = [(10, 10), (10,9), (10,8)]
+    create_snake(stdscr, snake)
     
     curses.curs_set(0)
     stdscr.timeout(0)
@@ -50,26 +58,19 @@ def main(stdscr_d):
     stdscr.refresh()
     
     #Ham... ham... apple
-    ay, ax = create_apple(stdscr)
+    create_apple(stdscr)
 
     key = curses.KEY_RIGHT
     last_key = key
 
-    while True:
-        time.sleep(0.1 - (0.01 * difficult))
-        c = stdscr.getch()
+    while True:        
+        stdscr.timeout(int(100 - (7 * difficult)))
+        #c = stdscr.getch()
+        key = stdscr.getch()
+        stdscr.timeout(0)
         
-        if c != -1:
-            key = c
-        
-        if stdscr.inch(ay, ax) != 320:
-            ay, ax = create_apple(stdscr)
-            score += (3 * difficult)
-            info.addstr(2, 2, 'Score: %d' % (score))
-            info.addstr(3, 2, 'Snake Lenght: %d' % (len(snake)))
-            #info.addstr(4, 2, 'Difficult: %d' % (difficult))
-            info.refresh()
-
+        #if c != -1:
+            #key = c
 
         if key == ord('p'):
             stdscr.timeout(-1)
@@ -79,63 +80,90 @@ def main(stdscr_d):
                                              curses.KEY_UP,
                                              curses.KEY_LEFT,
                                              curses.KEY_RIGHT, ord('q')) else last_key
+            last_key = key                                 
+            
 
         elif key in (ord('+'), ord('-')):
-            if key == ord('+'):
+            if key == ord('+') and difficult < 9:
                difficult += 1
 
-            else:
+            elif key == ord('-') and difficult > 1:
               difficult -= 1
-            
+
             info.addstr(4, 2, 'Difficult: %d' % (difficult))
             info.refresh()
+            key = last_key
+            
+        elif key == ord('q'):
+            sys.exit(0)
+            
+        elif curses.ascii.isascii(key):
             key = last_key
 
         else:
            last_key = key
 
-        
-        clean_snake(stdscr, snake)
-        
-        if key == ord('q'):
-            break
 
-        elif key == curses.KEY_DOWN:
+        
+        if key == curses.KEY_DOWN:
             y = snake[0][0] + 1
             x = snake[0][1]
-            
+
         elif key == curses.KEY_UP:
             y = snake[0][0] - 1
             x = snake[0][1]
-                        
-            
+
         elif key == curses.KEY_LEFT:
             y = snake[0][0]
             x = snake[0][1] - 1
-            
+
         elif key == curses.KEY_RIGHT:
             y = snake[0][0]
             x = snake[0][1] + 1
 
-        #This is to logic to check if the snake hit the "wall" or itself
-        if (0 >= x or x >= width -1 or 
-            0 >= y  or y >= height -1 or
-            (y, x) in snake):
-            stdscr.timeout(-1)
+        #Always include a new piece in the snake
+        snake.insert(0, (y, x))
+        
+        #Remove the last piece if not ate the apple
+        current_char = stdscr.inch(y, x)
+        if current_char == 32:
+            update_snake(stdscr, snake)
+            snake.pop()
+        
+        #Eating the apple... it's delicious
+        elif current_char == 320:
+            create_apple(stdscr)
+            stdscr.addch(y, x, '@')
+            score += (3 * difficult)
+            info.addstr(2, 2, 'Score: %d' % (score))
+            info.addstr(3, 2, 'Snake Lenght: %d' % (len(snake)))
+            info.refresh()
+            
+        else:
+            #Everything that's not a empty space or a red apple ends the game
+            update_snake(stdscr, snake)
+            snake.pop()
+            stdscr.timeout(400)
             info.addstr(12, 18, 'Game Over!!!')
             info.addstr(13, 17, 'Press q to quit')
+            info.addstr(14, 17, 'Press n to restart')
             info.refresh()
-            c = stdscr.getch()
-            while c != ord('q'):
+            update_snake(stdscr, snake)
+            stdscr.refresh()
+            c = None
+            while c not in (ord('q'), ord('n')):
+                #Blink effect, nice but this code is ugly
+                clean_snake(stdscr, snake)
                 c = stdscr.getch()
-            break
-        
-        snake.insert(0, (y, x))
-        if len(snake) > 1 and stdscr.inch(y, x) != 320:
-            snake.pop()            
+                create_snake(stdscr, snake)
+                c = c if c in (ord('q'), ord('n')) else stdscr.getch()
+                
+            if c == ord('q'):
+                sys.exit(0)
             
-        create_snake(stdscr, snake)
-        stdscr.refresh()
+            else:
+                stdscr_d.clear()
+                main(stdscr_d)
 
 
 if __name__ == '__main__':
